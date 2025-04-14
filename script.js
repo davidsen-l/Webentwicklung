67e5231c8d791787d8161daa80f5ce2ba7fcde01
Your Name <yournewemail@example.com>
2025-04-14 17:10:55 +0200

    funktion v2

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-/*let Buch = document.querySelector(".Buch");
-Buch.innerHTML += "<p><span>Buch</span></p>";*/
-
-let Buch = document.getElementById("new")
-Buch.addEventListener("click",add);
+let NewBuch = document.getElementById("new");
+NewBuch.addEventListener("click",add);
+inputValue = "Buch";
 function add(event){
-    /*let New = document.createElement("p");
+    let New = document.createElement("p"); // Schauen ob man das ins Buch tochter element reinbekommt
     New.textContent = inputValue;
-    Buch.append(New);*/
-    let Buch = document.querySelector(".Buch");
-    Buch.innerHTML += "<p><span>Buch</span></p>";
+    NewBuch.append(New);
+      /*let NewBuch = document.querySelector(".Buch");
+    NewBuch.innerHTML += "<p><span>Buch</span></p>";*/
 }
+let Buch = document.querySelector(".Buch");
