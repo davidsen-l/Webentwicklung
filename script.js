32e92dc5d994656029e45a564392efd58f7e2045
Y********* <y*********@gmail.com>
2025-05-03 18:16:02 +0200

    funktion v4

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-let NewBuch = document.getElementById("new");
+/*let NewBuch = document.getElementById("new");
 NewBuch.addEventListener("click",add);
 inputValue = "Buch";
 function add(event){
     /*let New = document.createElement("p"); // Schauen ob man das ins Buch tochter element reinbekommt
     New.textContent = inputValue;
-    NewBuch.append(New);*/
+    NewBuch.append(New);
     let NewBuch = document.querySelector(".Buch");
     NewBuch.innerHTML += "<p><span>Buch</span></p>";
-}
-let Buch = document.querySelector(".Buch");
-//const Buch = document.querySelector(".Buch");
-Buch.addEventListener("click", buchausleihen);
+}*/
+let Buch = document.querySelector('.Buch');
+let Popup = document.querySelector('.popupbuch');
+let Speichern = document.querySelector('.popupbuch button');
+let NameInput = document.querySelector('.popupbuch input');
+let PopupForm = document.querySelector('.popupbuch form');
+let Rückgabe = document.querySelector('.container');
+let GeklicktesBuch = null;
+
+Buch.addEventListener('click', buchausleihen);
+Speichern.addEventListener('click',abgabe);
+PopupForm.addEventListener('submit',function(event){event.preventDefault();})
+
 function buchausleihen(event){
-  //Soll n Popup geben wo mein sein Name eintragen kann
-  document.body.classList.add(".rentet");//soll das Buch als ausgeliehen markieren
+  let geklickt = event.target.closest('p');//mit hilfe von KI
+  GeklicktesBuch = geklickt;
+  if (geklickt.classList.contains('rentet')) {
+    Rückgabe.removeAttribute('hidden');// mein Button tauvht nicht auf
+  } else {
+    Popup.removeAttribute('hidden');
+  }
+}
+function abgabe (){
+  GeklicktesBuch.classList.add('rentet');
+  Popup.setAttribute('hidden','');
+  NameInput.value = '';
   // soll den Namen in die Liste packen
 }
+
+
 const person = {
-  book: "Annakarenina",
-  name: "Thomas Müller",
-  date: "20.03.2025",
+  book: 'Annakarenina',
+  name: 'Thomas Müller',
+  date: '20.03.2025',
 }
 let personAsJson = JSON.stringify(person);
 console.log(personAsJson);
 let personParsedBack = JSON.parse(personAsJson);
 console.log(personParsedBack);
-localStorage.setItem("person", personAsJson);
-console.log(localStorage.getItem("person"));
+localStorage.setItem('person', personAsJson);
+console.log(localStorage.getItem('person'));
