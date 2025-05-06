26b56d1ead52589e9ae3c52a07c313dc2b1c47ff
Y********* <y*********@gmail.com>
2025-05-06 21:52:55 +0200

    funktion v6

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function add(event){
     let NewBuch = document.querySelector(".Buch");
     NewBuch.innerHTML += "<p><span>Buch</span></p>";
 }*/
+class Person {
+  constructor(book, name, date) {
+    this.book = book;
+    this.name = name;
+    this.date = date;
+  }
+}
 let Buch = document.querySelector('.Buch');
 let Popup = document.querySelector('.popupbuch');
 let Speichern = document.querySelector('.popupbuch button');
@@ -15,17 +22,20 @@ let NameInput = document.querySelector('.popupbuch input');
 let PopupForm = document.querySelector('.popupbuch form');
 let Rückgabe = document.querySelector('.button');
 let GeklicktesBuch = null;
+let heute = new Date();
+let datum = heute.toLocaleDateString('de-DE');//mit Hilfe von KI
 
-Buch.addEventListener('click', rent);
+Buch.addEventListener('click',rent);
 Speichern.addEventListener('click',save);
 PopupForm.addEventListener('submit',function(event){event.preventDefault();})
 Rückgabe.addEventListener('click',giveback);
 
+
 function rent(event){
   let Geklickt = event.target.closest('p');//mit hilfe von KI
   GeklicktesBuch = Geklickt;
-  if (geklickt.classList.contains('rentet')) {
-    Rückgabe.removeAttribute('hidden');// mein Button tauvht nicht auf
+  if (Geklickt.classList.contains('rentet')) {
+    Rückgabe.removeAttribute('hidden');
   } else {
     Popup.removeAttribute('hidden');
   }
@@ -34,23 +44,19 @@ function rent(event){
 function save (){
   GeklicktesBuch.classList.add('rentet');
   Popup.setAttribute('hidden','');
-  NameInput.value = '';
+  let neuePerson = new Person (GeklicktesBuch.textContent,NameInput.value,heute);
+  NameInput.value= '';
   // soll den Namen in die Liste packen
 }
 
 function giveback (){
-  GeklicktesBuch.classList.remove('rentet');
   Rückgabe.setAttribute('hidden','');
+  GeklicktesBuch.classList.remove('rentet');
 }
 
-const person = {
-  book: 'Annakarenina',
-  name: 'Thomas Müller',
-  date: '20.03.2025',
-}
-let personAsJson = JSON.stringify(person);
+/*let personAsJson = JSON.stringify(neuePerson);
 console.log(personAsJson);
 let personParsedBack = JSON.parse(personAsJson);
 console.log(personParsedBack);
 localStorage.setItem('person', personAsJson);
-console.log(localStorage.getItem('person'));
+console.log(localStorage.getItem('person'));*/
